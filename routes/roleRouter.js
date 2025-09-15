f0ee941152e99999a5968c0d856227a139c82ca6
sahad <sahadmosco@gmail.com>
2025-09-16 01:38:37 +0400

    set role router protected with middleware

--- routes/roleRouter.js
+++ routes/roleRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createRole, getRole } from "../controllers/roleController.js";
+import { isLogged,checkAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getRole);
-router.post("/", createRole);
+router.post("/", isLogged,checkAdmin,createRole);
 
 export default router;
